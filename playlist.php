96336e8ff3e63f2d5141e988f7f0da42fcf345bb
Republic TV <gamerepublic24@gmail.com>
2026-09-24 07:04:44 +0800

    Auto Update M3U: Update Source 2

--- playlist.php
+++ playlist.php
@@ -107,7 +107,7 @@ http://193.47.62.44/hls/grrr.m3u8
 #EXTINF:-1 tvg-id="" tvg-name="Curico Unido vs D. Concepcion" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Ffootball%2Fcompetition%2Fd63f2665f489d5a1c0a059feb0b0d0e4.png%21w80&size=300&scale=1.5", Curico Unido vs D. Concepcion (Cúp Chile)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
 #EXTVLCOPT:http-referrer=https://www.colatv88xd.cc/
-https://live.dyrur1.com/live/74695702_78e0cf2754e815cb226ed81d76399595_autoChange.m3u8?auth_key=1790215364-0-0-229d4c041b89256a28ab6a3f32ba5e22
+https://live.dyrur1.com/live/74695702_78e0cf2754e815cb226ed81d76399595_autoChange.m3u8?auth_key=1790218145-0-0-350f18d126895ea3ed8fdaf76ef13415
 #EXTINF:-1 tvg-id="" tvg-name="BLV MOUNTAIN DEW" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Ffootball%2Fcompetition%2Fd63f2665f489d5a1c0a059feb0b0d0e4.png%21w80&size=300&scale=1.5", Curico Unido vs D. Concepcion - BLV MOUNTAIN DEW
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
 #EXTVLCOPT:http-referrer=https://live5.msrktz.app/
@@ -117,26 +117,21 @@ https://live05.meung.app/live/87547578_tsc.m3u8
 #EXTVLCOPT:http-referrer=https://xl365.domainkqt.cc/
 #EXTVLCOPT:http-origin=https://xl365.domainkqt.cc
 https://live1.quickscoreboardz.com/live/channel17.flv?wsSecret=284bfbae96c416a804a2cd3e75721e5b&wsABSTime=1789950591
-#EXTINF:-1 tvg-id="4647226-78988" tvg-name="Argentine Reserve League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Argentine Reserve League - 24 Sep 2026 04:00 WIB - Lanus Reserves vs Banfield Reserves (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4647226.m3u8?txSecret=4620771fdffe6bf38fc91acdf393e586&txTime=6AB59DD8
 #EXTINF:-1 tvg-id="4641689-79099" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 05:00 WIB - Curico Unido vs D. Concepcion (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4641689.m3u8?txSecret=5666dbe1596ee20b88fa95c2c28f9947&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-1-4641689.m3u8?txSecret=323ec04983c807f4f3d7163a097bb9bf&txTime=6AB5A8A0
 #EXTINF:-1 tvg-id="4641689-79088" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 05:00 WIB - Curico Unido vs D. Concepcion (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://live.dyrur1.com
 #EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/74695702_78e0cf2754e815cb226ed81d76399595_1080p.m3u8?auth_key=1790214794-0-0-f82e1330aea527f11243be27e9cda70f
+https://live.dyrur1.com/live/74695702_78e0cf2754e815cb226ed81d76399595_1080p.m3u8?auth_key=1790215558-0-0-63ce5859de7a0a27d6a38202b5ea44c7
 #EXTINF:-1 tvg-id="4649925-78959" tvg-name="Liga Dominicana de Fútbol" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Liga Dominicana de Fútbol - 24 Sep 2026 06:00 WIB - Moca FC vs Delfines Del Este (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4649925.m3u8?txSecret=0f8025fc9932399400d4128be72950bf&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-1-4649925.m3u8?txSecret=4b76e0d268922f84591ab33cb756e3b8&txTime=6AB5A8A0
 #EXTINF:-1 tvg-id="4649925-79101" tvg-name="Liga Dominicana de Fútbol" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Liga Dominicana de Fútbol - 24 Sep 2026 06:00 WIB - Moca FC vs Delfines Del Este (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
@@ -146,50 +141,63 @@ https://live2.dyrur1.com/live/14345543_92a9847eca45b34ef8ea74075d9a3d8c_1080p.m3
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4649926.m3u8?txSecret=4427d9b5178519579ae2a089c5f561b4&txTime=6AB59DD8
-#EXTINF:-1 tvg-id="4649926-79100" tvg-name="Liga Dominicana de Fútbol" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Liga Dominicana de Fútbol - 24 Sep 2026 06:00 WIB - Atletico Vega Real vs Jarabacoa FC (HD-J)
+https://pul-tenm.gkykp.com/live/sd-1-4649926.m3u8?txSecret=9a926ccfcff47f1c73c36c0cc9ae4beb&txTime=6AB5A8A0
+#EXTINF:-1 tvg-id="4649926-79102" tvg-name="Liga Dominicana de Fútbol" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Liga Dominicana de Fútbol - 24 Sep 2026 06:00 WIB - Atletico Vega Real vs Jarabacoa FC (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/74695702_78e0cf2754e815cb226ed81d76399595_1080p.m3u8?auth_key=1790214794-0-0-f82e1330aea527f11243be27e9cda70f
+#EXTVLCOPT:http-referrer=https://live1.dyrur1.com
+#EXTVLCOPT:http-origin=https://live1.dyrur1.com
+https://live1.dyrur1.com/live/14345544_7ef16e6eac908c78c816b491350bb859_1080p.m3u8?auth_key=1790216178-0-0-e161bb2b7582a7515ea42aa4c7a81ea4
 #EXTINF:-1 tvg-id="4642636-78947" tvg-name="CONCACAF Nations League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",CONCACAF Nations League - 24 Sep 2026 07:00 WIB - Aruba vs Antigua and Barbuda (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4642636.m3u8?txSecret=f4b8644242fc0efdb97b468577c483fd&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-1-4642636.m3u8?txSecret=6def9eae0a32ad336e668b8f14f288b7&txTime=6AB5A8A0
+#EXTINF:-1 tvg-id="4642636-79103" tvg-name="CONCACAF Nations League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",CONCACAF Nations League - 24 Sep 2026 07:00 WIB - Aruba vs Antigua and Barbuda (HD-J)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
+#EXTVLCOPT:http-referrer=https://live.dyrur1.com
+#EXTVLCOPT:http-origin=https://live.dyrur1.com
+https://live.dyrur1.com/live/74695702_78e0cf2754e815cb226ed81d76399595_1080p.m3u8?auth_key=1790215558-0-0-63ce5859de7a0a27d6a38202b5ea44c7
 #EXTINF:-1 tvg-id="3909064-78989" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 08:00 WIB - Atlanta Dream vs New York Liberty (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3909064.m3u8?txSecret=3ede23648e7e6019d3e90b17af3f0cf9&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-2-3909064.m3u8?txSecret=020fd82e5e24a77f7e3ed94f0792faa0&txTime=6AB5A8A0
 #EXTINF:-1 tvg-id="4649858-79093" tvg-name="Colombian Liga Betplay Femenina" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Colombian Liga Betplay Femenina - 24 Sep 2026 08:00 WIB - Independiente Santa Fe (W) vs Deportivo Cali Women (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4649858.m3u8?txSecret=09f567508f7f8026f6bfbb5aa0b963dd&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-1-4649858.m3u8?txSecret=e14430bae438ed349db0f022af8c896f&txTime=6AB5A8A0
 #EXTINF:-1 tvg-id="4640959-78990" tvg-name="Categoría Primera A" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Categoría Primera A - 24 Sep 2026 08:30 WIB - America de Cali vs Aguilas Doradas (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4640959.m3u8?txSecret=88053b46e23ae5404937a6fa54ad5650&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-1-4640959.m3u8?txSecret=dad009c4e73545f353d51ff48d7b1198&txTime=6AB5A8A0
 #EXTINF:-1 tvg-id="3909097-78993" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 10:00 WIB - Dallas Wings vs Seattle Storm (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3909097.m3u8?txSecret=880b1b2fc6922e0b4f48231976885834&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-2-3909097.m3u8?txSecret=86702591f9c1d60bd524329c878c24b4&txTime=6AB5A8A0
 #EXTINF:-1 tvg-id="4623502-78777" tvg-name="WAFF Arabian Gulf Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",WAFF Arabian Gulf Cup - 24 Sep 2026 23:55 WIB - United Arab Emirates vs Yemen (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4623502.m3u8?txSecret=438caf2cda96592c86e664cac2cc0d27&txTime=6AB59DD8
+https://pul-tenm.gkykp.com/live/sd-1-4623502.m3u8?txSecret=51a2098ecb863d23d0c64dc971d69b65&txTime=6AB5A8A0
 #EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/2fa59a9c9801ee88b9049fa051fce162.png!w80",08:00 | Lanzhou Longyuan vs Yichun Grand Tiger
 https://live05.meung.app/live/87547578_tsc.m3u8
 #EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/1fced0158de1169e9bc3209f4ab98e84.png!w80",15:00 | CLB KuPs vs Ilves
 https://live05.meung.app/live/87547578.m3u8
+#EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/15b9e3f5c163260c1e77e246dcb58cfd.png!w80",12:00 | Chengdu Rongcheng B vs Guangzhou Dandelion Alpha FC
+https://live05.meung.app/live/75748097.m3u8
+#EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/d639bb3f26ed6f1600de1e55885fa4be.png!w80",15:00 | FK Andijon vs Buxoro FK
+https://live05.meung.app/live/75748097.m3u8
 #EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/9ba1f45cab5e3f52d783a5bfdbe10daa.png!w80",18:00 | Boca Juniors Nữ vs San Lorenzo Nữ
 https://live05.meung.app/live/87547578.m3u8
 #EXTINF:-1 group-title="SPORT-TV" tvg-logo="",18:00 27/07 Singapore vs Timor Leste (BLV VƯƠNG LUÂN) [hls]
 https://freem3u.xyz/static/no-signal/low.m3u8
+#EXTINF:-1 group-title="SPORT-TV" tvg-logo="",22:00 27/07 FK Andijon vs Buxoro FK (BLV SPRITE) [flv]
+https://live05.meung.app/live/75748097.flv
+#EXTINF:-1 group-title="SPORT-TV" tvg-logo="",22:00 27/07 FK Andijon vs Buxoro FK (BLV SPRITE) [hls 2]
+https://live05.miekgo.app/live/75748097.m3u8
 #EXTINF:-1 group-title="SPORT-TV" tvg-logo="",01:00 28/07 Boca Juniors Nữ vs San Lorenzo Nữ (BLV MOUNTAIN DEW) [flv]
 https://live05.meung.app/live/87547578.flv
 #EXTINF:-1 group-title="SPORT-TV" tvg-logo="",01:00 28/07 Boca Juniors Nữ vs San Lorenzo Nữ (BLV MOUNTAIN DEW) [hls 2]
