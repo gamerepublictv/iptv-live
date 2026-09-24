0893b0f6a69bdc990cdc10a4fe3fe4b4cdc100b4
Republic TV <gamerepublic24@gmail.com>
2026-09-24 09:16:00 +0800

    Auto Update M3U: Update Source 2

--- playlist.php
+++ playlist.php
@@ -147,97 +147,103 @@ http://193.47.62.59/hls/IQQQEEEEQAAAAA.m3u8
 #EXTINF:-1 tvg-id="" tvg-name="Municipal Iquique vs Antofagasta" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Ffootball%2Fcompetition%2Fd63f2665f489d5a1c0a059feb0b0d0e4.png%21w80&size=300&scale=1.5", Municipal Iquique vs Antofagasta (Cúp Chile)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
 #EXTVLCOPT:http-referrer=https://www.colatv88xd.cc/
-https://live.dyrur1.com/live/74321588_babee9dc13c03fc5ff96d4c90b69521f_autoChange.m3u8?auth_key=1790223274-0-0-40588fb4585ea903e75d2181e16b6f6f
+https://live.dyrur1.com/live/74321588_babee9dc13c03fc5ff96d4c90b69521f_autoChange.m3u8?auth_key=1790225947-0-0-b6f705bbea23b3807959d5b4300181cf
 #EXTINF:-1 tvg-id="" tvg-name="BLV SPRITE" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Ffootball%2Fcompetition%2Fd63f2665f489d5a1c0a059feb0b0d0e4.png%21w80&size=300&scale=1.5", Municipal Iquique vs Antofagasta - BLV SPRITE
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
 #EXTVLCOPT:http-referrer=https://live5.msrktz.app/
 https://live05.meung.app/live/75748097_tsc.m3u8
-#EXTINF:-1 tvg-id="" tvg-name="New York Liberty vs Atlanta Dream" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Fbasketball%2Fcompetition%2F86f522333da4c3e2c144996fc4d2520b.png%21w80&size=300&scale=1.5", New York Liberty vs Atlanta Dream (Hiệp hội bóng rổ quốc gia nữ)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
-#EXTVLCOPT:http-referrer=https://www.colatv88xd.cc/
-https://live.dyrur1.com/live/68096422_f5cbbb7080cc12398b3b2f07d700b64f_autoChange.m3u8?auth_key=1790223275-0-0-cef7efdd7063a6546e873760a9979be0
-#EXTINF:-1 tvg-id="" tvg-name="BLV Cozy" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Fbasketball%2Fcompetition%2F86f522333da4c3e2c144996fc4d2520b.png%21w80&size=300&scale=1.5", New York Liberty vs Atlanta Dream - BLV Cozy
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
-#EXTVLCOPT:http-referrer=https://live5.msrktz.app/
-https://live05.meung.app/live/87379114_tsc.m3u8
 #EXTINF:-1 tvg-id="" tvg-name="CLB America de Cali vs CLB Aguilas Doradas" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Ffootball%2Fcompetition%2Fa43b934409a4d2475b0e5f69d1b2ec16.png%21w80&size=300&scale=1.5", CLB America de Cali vs CLB Aguilas Doradas (Giải vô địch quốc gia Colombia)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
 #EXTVLCOPT:http-referrer=https://www.colatv88xd.cc/
-https
+https://live.dyrur1.com/live/72335142_f6697aa8f11bc4d8f414e79e33ac4996_autoChange.m3u8?auth_key=1790225947-0-0-d5a0eafa1b4be53364427f518327165e
 #EXTINF:-1 tvg-id="" tvg-name="BLV BÍ ĐAO" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Ffootball%2Fcompetition%2Fa43b934409a4d2475b0e5f69d1b2ec16.png%21w80&size=300&scale=1.5", CLB America de Cali vs CLB Aguilas Doradas - BLV BÍ ĐAO
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
 #EXTVLCOPT:http-referrer=https://live5.msrktz.app/
 https://live05.meung.app/live/99121525_tsc.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="New York Liberty vs Atlanta Dream" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Fbasketball%2Fcompetition%2F86f522333da4c3e2c144996fc4d2520b.png%21w80&size=300&scale=1.5", New York Liberty vs Atlanta Dream (Hiệp hội bóng rổ quốc gia nữ)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
+#EXTVLCOPT:http-referrer=https://www.colatv88xd.cc/
+https://live.dyrur1.com/live/68096422_f5cbbb7080cc12398b3b2f07d700b64f_autoChange.m3u8?auth_key=1790225947-0-0-c286e1477d65088856a271654aace5c2
+#EXTINF:-1 tvg-id="" tvg-name="BLV Cozy" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Fbasketball%2Fcompetition%2F86f522333da4c3e2c144996fc4d2520b.png%21w80&size=300&scale=1.5", New York Liberty vs Atlanta Dream - BLV Cozy
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
+#EXTVLCOPT:http-referrer=https://live5.msrktz.app/
+https://live05.meung.app/live/87379114_tsc.m3u8
 #EXTINF:-1 group-title="Event-LIVE-NOW" tvg-logo="https://openwindows037.serv00.net/logo/liga_bri.png" ,BRI Super League - Persijap vs Persib
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:156.0) Gecko/20100101 Firefox/156.0
 #EXTVLCOPT:http-referrer=https://xl365.domainkqt.cc/
 #EXTVLCOPT:http-origin=https://xl365.domainkqt.cc
 https://live1.quickscoreboardz.com/live/channel17.flv?wsSecret=284bfbae96c416a804a2cd3e75721e5b&wsABSTime=1789950591
-#EXTINF:-1 tvg-id="3943500-79113" tvg-name="Brazil Federação Paulista de Basquete" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Brazil Federação Paulista de Basquete - 24 Sep 2026 07:00 WIB - Corinthians vs Mogi das Cruzes (HD-J)
+#EXTINF:-1 tvg-id="4641691-79105" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 07:30 WIB - Municipal Iquique vs CSD Antofagasta (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://live1.dyrur1.com
 #EXTVLCOPT:http-origin=https://live1.dyrur1.com
-https://live1.dyrur1.com/live/14447883_fc10b52de0dcc77cb1f06c6dcca595a0_1080p.m3u8?auth_key=1790220145-0-0-6162feb0cb9531627a0f7b1a911bbf36
-#EXTINF:-1 tvg-id="4642636-78947" tvg-name="CONCACAF Nations League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",CONCACAF Nations League - 24 Sep 2026 07:00 WIB - Aruba vs Antigua and Barbuda (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4642636.m3u8?txSecret=e5efef14027f44211e8349fdeb53d861&txTime=6AB5BC50
-#EXTINF:-1 tvg-id="4642636-79103" tvg-name="CONCACAF Nations League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",CONCACAF Nations League - 24 Sep 2026 07:00 WIB - Aruba vs Antigua and Barbuda (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/73220994_1cd0d3f7900f77f6bdbf84ff62b7369e_1080p.m3u8?auth_key=1790222663-0-0-68610990dce54ca0b704714fe35c640b
-#EXTINF:-1 tvg-id="4641691-79115" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 07:30 WIB - Municipal Iquique vs CSD Antofagasta (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4641691.m3u8?txSecret=e45b6e2431bbad6e9f513b4b047aa961&txTime=6AB5BC50
-#EXTINF:-1 tvg-id="4641691-79105" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 07:30 WIB - Municipal Iquique vs CSD Antofagasta (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/73220994_1cd0d3f7900f77f6bdbf84ff62b7369e_1080p.m3u8?auth_key=1790222663-0-0-68610990dce54ca0b704714fe35c640b
+https://live1.dyrur1.com/live/74321590_f0c24c35c68fe7d534d8d1a58e903fcd_1080p.m3u8?auth_key=1790225624-0-0-ac15875397d0705e582acaf4179b8bb4
 #EXTINF:-1 tvg-id="4641693-79106" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 07:30 WIB - Union La Calera vs Univ Catolica (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4641693.m3u8?txSecret=4fb00715d0af0c503fbbd475e65516e9&txTime=6AB5BC50
+https://pul-tenm.gkykp.com/live/sd-1-4641693.m3u8?txSecret=9049c81ee85c0bbd9e8875291b9eaf13&txTime=6AB5C718
 #EXTINF:-1 tvg-id="4641693-79104" tvg-name="Chilean Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Chilean Cup - 24 Sep 2026 07:30 WIB - Union La Calera vs Univ Catolica (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/73220994_1cd0d3f7900f77f6bdbf84ff62b7369e_1080p.m3u8?auth_key=1790222663-0-0-68610990dce54ca0b704714fe35c640b
+#EXTVLCOPT:http-referrer=https://live1.dyrur1.com
+#EXTVLCOPT:http-origin=https://live1.dyrur1.com
+https://live1.dyrur1.com/live/74321590_f0c24c35c68fe7d534d8d1a58e903fcd_1080p.m3u8?auth_key=1790225624-0-0-ac15875397d0705e582acaf4179b8bb4
 #EXTINF:-1 tvg-id="3909064-78989" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 08:00 WIB - Atlanta Dream vs New York Liberty (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/hd-en-2-3909064.m3u8?txSecret=4ef267e84b845439f204002155bef8bb&txTime=6AB5BC50
+https://pul-tenm.gkykp.com/live/hd-en-2-3909064.m3u8?txSecret=a515fe1a393c2788321a94cf8340ad30&txTime=6AB5C718
 #EXTINF:-1 tvg-id="3909064-79114" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 08:00 WIB - Atlanta Dream vs New York Liberty (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://live.dyrur1.com
 #EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/68096422_f5cbbb7080cc12398b3b2f07d700b64f_1080p.m3u8?auth_key=1790223054-0-0-a79b910bd6ad7a2a2ef10d4fa4ddd1c6
-#EXTINF:-1 tvg-id="4649858-79117" tvg-name="Colombian Liga Betplay Femenina" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Colombian Liga Betplay Femenina - 24 Sep 2026 08:00 WIB - Independiente Santa Fe (W) vs Deportivo Cali Women (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/73220994_1cd0d3f7900f77f6bdbf84ff62b7369e_1080p.m3u8?auth_key=1790222663-0-0-68610990dce54ca0b704714fe35c640b
-#EXTINF:-1 tvg-id="4640959-78990" tvg-name="Categoría Primera A" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Categoría Primera A - 24 Sep 2026 08:30 WIB - America de Cali vs Aguilas Doradas (HD-B)
+https://live.dyrur1.com/live/68096422_f5cbbb7080cc12398b3b2f07d700b64f_1080p.m3u8?auth_key=1790225886-0-0-6b63afcaf8f9bf67b8a57eb1293d699f
+#EXTINF:-1 tvg-id="4640959-79124" tvg-name="Categoría Primera A" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Categoría Primera A - 24 Sep 2026 08:30 WIB - America de Cali vs Aguilas Doradas (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4640959.m3u8?txSecret=27e8a206c9b815ff925055943d129bfc&txTime=6AB5BC50
+https://pul-tenm.gkykp.com/live/sd-1-4640959.m3u8?txSecret=8d8e939540ad3c9efccc9c9fb79bb1e1&txTime=6AB5C718
+#EXTINF:-1 tvg-id="4640959-79125" tvg-name="Categoría Primera A" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Categoría Primera A - 24 Sep 2026 08:30 WIB - America de Cali vs Aguilas Doradas (HD-J)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
+#EXTVLCOPT:http-referrer=https://live.dyrur1.com
+#EXTVLCOPT:http-origin=https://live.dyrur1.com
+https://live.dyrur1.com/live/72335142_f6697aa8f11bc4d8f414e79e33ac4996_1080p.m3u8?auth_key=1790225832-0-0-099ee081b014b1c436e424c4971a31ed
+#EXTINF:-1 tvg-id="3943473-79119" tvg-name="Asian Games - Women's Basketball" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Asian Games - Women's Basketball - 24 Sep 2026 09:00 WIB - South Korea Women vs Indonesia Women (HD-B)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
+#EXTVLCOPT:http-referrer=https://oneball.live/
+#EXTVLCOPT:http-origin=https://oneball.live
+https://pul-tenm.gkykp.com/live/hd-en-2-3943473.m3u8?txSecret=a36aede6b055c5aa64538413c1f98ed0&txTime=6AB5C718
+#EXTINF:-1 tvg-id="3943473-79121" tvg-name="Asian Games - Women's Basketball" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Asian Games - Women's Basketball - 24 Sep 2026 09:00 WIB - South Korea Women vs Indonesia Women (HD-J)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
+#EXTVLCOPT:http-referrer=https://live1.dyrur1.com
+#EXTVLCOPT:http-origin=https://live1.dyrur1.com
+https://live1.dyrur1.com/live/74321590_f0c24c35c68fe7d534d8d1a58e903fcd_1080p.m3u8?auth_key=1790225624-0-0-ac15875397d0705e582acaf4179b8bb4
 #EXTINF:-1 tvg-id="3909097-78993" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 10:00 WIB - Dallas Wings vs Seattle Storm (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3909097.m3u8?txSecret=7dd52119999038896f28938b286316cb&txTime=6AB5BC50
+https://pul-tenm.gkykp.com/live/sd-2-3909097.m3u8?txSecret=7855d483ccbfe77defa62ad529e344e3&txTime=6AB5C718
 #EXTINF:-1 tvg-id="4623502-78777" tvg-name="WAFF Arabian Gulf Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",WAFF Arabian Gulf Cup - 24 Sep 2026 23:55 WIB - United Arab Emirates vs Yemen (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4623502.m3u8?txSecret=4ed8c6a208b4337d7ce99eec5eb46626&txTime=6AB5BC50
+https://pul-tenm.gkykp.com/live/sd-1-4623502.m3u8?txSecret=2cd6666b05137d5410da79ef8eabbdc4&txTime=6AB5C718
+#EXTINF:-1 group-logo="https://thumbor.prod.vidiocdn.com/K0imcBk5BgpBtjY9UYBkIrMDKFY=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329746/38ec04.png" tvg-logo="https://thumbor.prod.vidiocdn.com/K0imcBk5BgpBtjY9UYBkIrMDKFY=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329746/38ec04.png" group-title="Event-LIVE-NOW", Indonesia vs Malaysia- Men's Sepaktakraw Team Regu Semifinal | 20th Asian Games Aichi-Nagoya 2026 (06:50 - 09:00 * 24 September 2026)
+#EXTVLCOPT:http-referrer=https://m.vidio.com
+#EXTVLCOPT:http-user-agent=Dalvik/2.1.0 (Linux; U; Android 12; sti6140d360 Build/QT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 TV Safari/537.36
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=dash
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://dusk.biz.id/fidyu/play.drm?id=22241&type=drm&token=8aac158025557f4853fdf619fd43998a
+https://dusk.biz.id/fidyu/play.mpd?id=22241&type=dash&token=8aac158025557f4853fdf619fd43998a
+#EXTINF:-1 group-logo="https://thumbor.prod.vidiocdn.com/UelLg6BuRju1DXEpqmcA897njIU=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329750/053983.jpg" tvg-logo="https://thumbor.prod.vidiocdn.com/UelLg6BuRju1DXEpqmcA897njIU=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329750/053983.jpg" group-title="Event-LIVE-NOW", Korea vs Indonesia - Women's Basketball Quarterfinals | 20th Asian Games Aichi-Nagoya 2026 (07:48 - 10:15 * 24 September 2026)
+#EXTVLCOPT:http-referrer=https://m.vidio.com
+#EXTVLCOPT:http-user-agent=Dalvik/2.1.0 (Linux; U; Android 12; sti6140d360 Build/QT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 TV Safari/537.36
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=dash
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://dusk.biz.id/fidyu/play.drm?id=22240&type=drm&token=8aac158025557f4853fdf619fd43998a
+https://dusk.biz.id/fidyu/play.mpd?id=22240&type=dash&token=8aac158025557f4853fdf619fd43998a
 #EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/0ab1b8407086bab060a396f244a28cd6.png!w80",15:00 | CLB Lahti vs TPS
 https://live05.meung.app/live/99121525.m3u8
 #EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/15b9e3f5c163260c1e77e246dcb58cfd.png!w80",12:00 | Chengdu Rongcheng B vs Guangzhou Dandelion Alpha FC
