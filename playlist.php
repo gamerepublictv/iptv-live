cb12cd76acb796161844c1d138900055038f208b
Republic TV <gamerepublic24@gmail.com>
2026-09-24 12:56:50 +0800

    Auto Update M3U: Update Source 2

--- playlist.php
+++ playlist.php
@@ -64,99 +64,61 @@ http://193.47.62.44/hls/RARARAD.m3u8
 #EXTVLCOPT:http-origin=http://www.fawanews.sc
 #EXTVLCOPT:http-referrer=http://www.fawanews.sc/
 http://193.47.62.44/hls/RARARA.m3u8
-#EXTINF:-1 tvg-id="" tvg-name="Seattle Storm vs Dallas Wings" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Fbasketball%2Fcompetition%2F86f522333da4c3e2c144996fc4d2520b.png%21w80&size=300&scale=1.5", Seattle Storm vs Dallas Wings (Hiệp hội bóng rổ quốc gia nữ)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
-#EXTVLCOPT:http-referrer=https://www.colatv88xd.cc/
-https://live.dyrur1.com/live/o17p8syddl0hykj_2eb82960d03f55ca47ebf182666e5ac4_autoChange.m3u8?auth_key=1790236287-0-0-12070aa20c453831544cbd267f0eacca
-#EXTINF:-1 tvg-id="" tvg-name="BLV POCARI" group-title="Event-LIVE-NOW" tvg-logo="https://gatotkaca007.serv00.net/cola/logo.php?src=https%3A%2F%2Fimg.gvapi.cc%2Fbasketball%2Fcompetition%2F86f522333da4c3e2c144996fc4d2520b.png%21w80&size=300&scale=1.5", Seattle Storm vs Dallas Wings - BLV POCARI
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 13)
-#EXTVLCOPT:http-referrer=https://live5.msrktz.app/
-https://live05.meung.app/live/19919577_tsc.m3u8
 #EXTINF:-1 group-title="Event-LIVE-NOW" tvg-logo="https://openwindows037.serv00.net/logo/liga_bri.png" ,BRI Super League - Persijap vs Persib
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:156.0) Gecko/20100101 Firefox/156.0
 #EXTVLCOPT:http-referrer=https://xl365.domainkqt.cc/
 #EXTVLCOPT:http-origin=https://xl365.domainkqt.cc
 https://live1.quickscoreboardz.com/live/channel17.flv?wsSecret=284bfbae96c416a804a2cd3e75721e5b&wsABSTime=1789950591
-#EXTINF:-1 tvg-id="3909097-79132" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 10:00 WIB - Dallas Wings vs Seattle Storm (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/hd-en-2-3909097.m3u8?txSecret=069e13c5dc6b3dc7aef7e44200f18bba&txTime=6AB5F058
-#EXTINF:-1 tvg-id="3909097-79135" tvg-name="Women's National Basketball Association" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Women's National Basketball Association - 24 Sep 2026 10:00 WIB - Dallas Wings vs Seattle Storm (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/o17p8syddl0hykj_2eb82960d03f55ca47ebf182666e5ac4_1080p.m3u8?auth_key=1790235117-0-0-88fe4afa57ecea539156c77aa2e33359
-#EXTINF:-1 tvg-id="4492317-79154" tvg-name="Canadian Premier League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Canadian Premier League - 24 Sep 2026 10:00 WIB - Vancouver FC vs Inter Toronto FC (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4492317.m3u8?txSecret=8224eb326796a353262e70c93be39903&txTime=6AB5F058
-#EXTINF:-1 tvg-id="4492317-79144" tvg-name="Canadian Premier League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Canadian Premier League - 24 Sep 2026 10:00 WIB - Vancouver FC vs Inter Toronto FC (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/68686122_1415941a02b67aca725a158d50dc2b09_1080p.m3u8?auth_key=1790230460-0-0-8ee47e62ba903c2e7665281867e4e42b
-#EXTINF:-1 tvg-id="4571951-79153" tvg-name="Guatemala Liga Nacional" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Guatemala Liga Nacional - 24 Sep 2026 10:00 WIB - Xelaju MC vs Antigua GFC (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4571951.m3u8?txSecret=1a5100784e841dbd4adce8c0a0e5cda9&txTime=6AB5F058
-#EXTINF:-1 tvg-id="4571951-79151" tvg-name="Guatemala Liga Nacional" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Guatemala Liga Nacional - 24 Sep 2026 10:00 WIB - Xelaju MC vs Antigua GFC (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/72474892_37662a0f63c8cb2208aca680d7cc9f46_1080p.m3u8?auth_key=1790231132-0-0-60b51688208c08d2100ae220569d5704
-#EXTINF:-1 tvg-id="3943571-79159" tvg-name="Club Friendship" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Club Friendship - 24 Sep 2026 10:40 WIB - Huaqiao University vs 黎明学院 (HD-B)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://oneball.live/
-#EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3943571.m3u8?txSecret=61e66bf4da08c17bf29622dc54d5884e&txTime=6AB5F058
-#EXTINF:-1 tvg-id="3943571-79160" tvg-name="Club Friendship" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Club Friendship - 24 Sep 2026 10:40 WIB - Huaqiao University vs 黎明学院 (HD-J)
-#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
-#EXTVLCOPT:http-referrer=https://live.dyrur1.com
-#EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/75000840_b917198ae3a757b7cc21a022da2054dd_1080p.m3u8?auth_key=1790236287-0-0-27d4d410b67a4363814e3934b55842ad
 #EXTINF:-1 tvg-id="3943474-79138" tvg-name="Asian Games - Women's Basketball" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Asian Games - Women's Basketball - 24 Sep 2026 12:00 WIB - Mongolia Women vs China Women (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/hd-en-2-3943474.m3u8?txSecret=e4effb9bb12582dd882ac387a4e88963&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/hd-en-2-3943474.m3u8?txSecret=67938cf74b2741201fde3fad4966e632&txTime=6AB5FAA8
 #EXTINF:-1 tvg-id="3943474-79161" tvg-name="Asian Games - Women's Basketball" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Asian Games - Women's Basketball - 24 Sep 2026 12:00 WIB - Mongolia Women vs China Women (HD-J)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://live.dyrur1.com
 #EXTVLCOPT:http-origin=https://live.dyrur1.com
-https://live.dyrur1.com/live/o17p8syddl0hykj_2eb82960d03f55ca47ebf182666e5ac4_1080p.m3u8?auth_key=1790235117-0-0-88fe4afa57ecea539156c77aa2e33359
+https://live.dyrur1.com/live/74977664_eb6c95d2f18b6f32e24b0e05772823ab_1080p.m3u8?auth_key=1790237962-0-0-59be237c3ff1af0d4e95d5de9e645170
+#EXTINF:-1 tvg-id="3943549-79193" tvg-name="Club Friendship" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Club Friendship - 24 Sep 2026 12:20 WIB - CTBC Business School Women vs University of Taipei(w) (HD-B)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
+#EXTVLCOPT:http-referrer=https://oneball.live/
+#EXTVLCOPT:http-origin=https://oneball.live
+https://pul-tenm.gkykp.com/live/sd-2-3943549.m3u8?txSecret=f87b60a4c4050379dcf4c995aa96171d&txTime=6AB5FAA8
+#EXTINF:-1 tvg-id="3943549-79194" tvg-name="Club Friendship" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Club Friendship - 24 Sep 2026 12:20 WIB - CTBC Business School Women vs University of Taipei(w) (HD-J)
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
+#EXTVLCOPT:http-referrer=https://live.dyrur1.com
+#EXTVLCOPT:http-origin=https://live.dyrur1.com
+https://live.dyrur1.com/live/74887864_9df81de959f7449217a3ad654c622b8b_1080p.m3u8?auth_key=1790238483-0-0-62d1e9638623f7886be00d6ea38f6680
 #EXTINF:-1 tvg-id="3943475-79139" tvg-name="Asian Games - Women's Basketball" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Asian Games - Women's Basketball - 24 Sep 2026 15:00 WIB - Chinese Taipei Women vs Philippines Women (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3943475.m3u8?txSecret=a63eb33e4b45b45c64249bbb15bb246e&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/sd-2-3943475.m3u8?txSecret=19c6f4fb7e0db0dda9911948cad8d4ff&txTime=6AB5FAA8
 #EXTINF:-1 tvg-id="3931124-79140" tvg-name="FIBA Intercontinental Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",FIBA Intercontinental Cup - 24 Sep 2026 15:30 WIB - Rytas Vilnius vs Shanghai Jiushi Sharks (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3931124.m3u8?txSecret=f91adc224d864a76abf4bfa100bc4055&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/sd-2-3931124.m3u8?txSecret=5b1bedc8e9638aabf267443ad2a0896b&txTime=6AB5FAA8
 #EXTINF:-1 tvg-id="3926016-79141" tvg-name="National Basketball League" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",National Basketball League - 24 Sep 2026 17:30 WIB - South East Melbourne Phoenix vs Melbourne United (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3926016.m3u8?txSecret=523f1e8f2847e0313d04ebfebe36b607&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/sd-2-3926016.m3u8?txSecret=970b2192726f09f7e2ef50cef1070702&txTime=6AB5FAA8
 #EXTINF:-1 tvg-id="3935536-79142" tvg-name="B.League Premier" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",B.League Premier - 24 Sep 2026 18:05 WIB - Gunma Crane Thunders vs Altiri Chiba (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3935536.m3u8?txSecret=50eead4184aeda86dcc530d1dc82e0ff&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/sd-2-3935536.m3u8?txSecret=5824bf7c0bb6a068f5a9c135d405c104&txTime=6AB5FAA8
 #EXTINF:-1 tvg-id="3943476-79143" tvg-name="Asian Games - Women's Basketball" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",Asian Games - Women's Basketball - 24 Sep 2026 18:20 WIB - Japan Women vs Thailand Women (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-2-3943476.m3u8?txSecret=d4a9c6adf95441bb333ee63d74b73a89&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/sd-2-3943476.m3u8?txSecret=bc1aea6196f22fe8ff49f19a19c744b4&txTime=6AB5FAA8
 #EXTINF:-1 tvg-id="4623502-78777" tvg-name="WAFF Arabian Gulf Cup" tvg-logo="https://openwindows037.serv00.net/logo/live_event2.png" group-title="Event-LIVE-NOW",WAFF Arabian Gulf Cup - 24 Sep 2026 23:55 WIB - United Arab Emirates vs Yemen (HD-B)
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0
 #EXTVLCOPT:http-referrer=https://oneball.live/
 #EXTVLCOPT:http-origin=https://oneball.live
-https://pul-tenm.gkykp.com/live/sd-1-4623502.m3u8?txSecret=629eb34eeefa14a2821c0090bd632c44&txTime=6AB5F058
+https://pul-tenm.gkykp.com/live/sd-1-4623502.m3u8?txSecret=8dc4bf48aaf6228ee43669f367909f9a&txTime=6AB5FAA8
 #EXTINF:-1 group-logo="https://thumbor.prod.vidiocdn.com/HNAbv2lwTysEYRU9CixGVXq2xZg=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5328611/94b231.png" tvg-logo="https://thumbor.prod.vidiocdn.com/HNAbv2lwTysEYRU9CixGVXq2xZg=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5328611/94b231.png" group-title="Event-LIVE-NOW", Ukraine vs Belgium - Billie Jean King Cup 2026 (09:00 - 15:00 * 24 September 2026)
 #EXTVLCOPT:http-referrer=https://m.vidio.com
 #EXTVLCOPT:http-user-agent=Dalvik/2.1.0 (Linux; U; Android 12; sti6140d360 Build/QT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 TV Safari/537.36
@@ -173,14 +135,22 @@ https://dusk.biz.id/fidyu/play.mpd?id=6685&type=dash&token=70717f5a6d8dcc366e716
 #KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
 #KODIPROP:inputstream.adaptive.license_key=https://dusk.biz.id/fidyu/play.drm?id=6686&type=drm&token=70717f5a6d8dcc366e716f6422ea4d8a
 https://dusk.biz.id/fidyu/play.mpd?id=6686&type=dash&token=70717f5a6d8dcc366e716f6422ea4d8a
-#EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/3cb5e2d3b6f734ff2f10e4b47e50d380.png!w80",16:00 | Jaro vs CLB Gnistan Helsinki
-https://live05.meung.app/live/19919577.m3u8
-#EXTINF:-1 group-title="FPT SPORT" tvg-logo="",22h00 ngày 23/06 KuPs vs Ilves Tampere [BLV ENZO] (Socolive) m3u8
-https://pull.niues.live/live/stream-380460_lsd.m3u8?auth_key=1782232522-0-0-56e4b93b1d457bb424d110ca6730a379
-#EXTINF:-1 group-title="FPT SPORT" tvg-logo="",22h00 ngày 23/06 KuPs vs Ilves Tampere [BLV ENZO] (Socolive) hdM3u8
-https://pull.niues.live/live/stream-380460_lhd.m3u8?auth_key=1782232522-0-0-817442f1c8516fe936675b737e2b61ef
-#EXTINF:-1 group-title="CO LA TV" tvg-logo="https://img.gvapi.cc/football/team/e142922645466d58b5ad3a27570b5ac5.png!w80",22:00 | CLB Banfield vs Sarmiento Junin
-https://live05.meung.app/live/19919577.m3u8
+#EXTINF:-1 group-logo="https://thumbor.prod.vidiocdn.com/rUoYWA-K8hcQGAsVJY0ksvQ5slE=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329805/59c4d6.png" tvg-logo="https://thumbor.prod.vidiocdn.com/rUoYWA-K8hcQGAsVJY0ksvQ5slE=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329805/59c4d6.png" group-title="Event-LIVE-NOW", China vs Indonesia - Men's 3x3 Basketball Play-in | 20th Asian Games Aichi-Nagoya 2026 (11:15 - 11:40 * 24 September 2026)
+#EXTVLCOPT:http-referrer=https://m.vidio.com
+#EXTVLCOPT:http-user-agent=Dalvik/2.1.0 (Linux; U; Android 12; sti6140d360 Build/QT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 TV Safari/537.36
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=dash
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://dusk.biz.id/fidyu/play.drm?id=22240&type=drm&token=70717f5a6d8dcc366e716f6422ea4d8a
+https://dusk.biz.id/fidyu/play.mpd?id=22240&type=dash&token=70717f5a6d8dcc366e716f6422ea4d8a
+#EXTINF:-1 group-logo="https://thumbor.prod.vidiocdn.com/gN7-clu2GQm9OqwXqRKsvsorfDU=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329768/932a27.png" tvg-logo="https://thumbor.prod.vidiocdn.com/gN7-clu2GQm9OqwXqRKsvsorfDU=/640x360/filters:strip_icc():quality(70)/vidio-media-production/uploads/livestreaming/schedule/thumbnail/5329768/932a27.png" group-title="Event-LIVE-NOW", Ricko Saputra (INA) - Men's 60kg Final | 20th Asian Games Aichi-Nagoya 2026 (11:20 - 14:00 * 24 September 2026)
+#EXTVLCOPT:http-referrer=https://m.vidio.com
+#EXTVLCOPT:http-user-agent=Dalvik/2.1.0 (Linux; U; Android 12; sti6140d360 Build/QT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 TV Safari/537.36
+#KODIPROP:inputstreamaddon=inputstream.adaptive
+#KODIPROP:inputstream.adaptive.manifest_type=dash
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=https://dusk.biz.id/fidyu/play.drm?id=22242&type=drm&token=70717f5a6d8dcc366e716f6422ea4d8a
+https://dusk.biz.id/fidyu/play.mpd?id=22242&type=dash&token=70717f5a6d8dcc366e716f6422ea4d8a
 #EXTINF:-1 group-title="SPORT-TV" tvg-logo="",18:00 27/07 Singapore vs Timor Leste (BLV VƯƠNG LUÂN) [hls]
 https://freem3u.xyz/static/no-signal/low.m3u8
 #EXTINF:-1 group-title="SPORT-TV" tvg-logo="",Cập Nhật
